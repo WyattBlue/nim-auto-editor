a879ecd8b38632724c9bd9ea2c3e7b26617b5c64
WyattBlue <wyattblue@auto-editor.com>
2023-06-20 09:16:31 +0200

    Add subtitle stream to info outout

--- src/subinfo.nim
+++ src/subinfo.nim
@@ -9,54 +9,80 @@ type
   StreamKind = enum
     VideoKind,
     AudioKind,
+    SubtitleKind,
+    ContainerKind,
 
   Stream = ref object
-    codec: string
-    lang: string
     duration: string
     bitrate: uint64
     case kind: StreamKind
     of VideoKind:
+      vcodec: string
       width: uint64
       height: uint64
       fps: string # Rational[int64]
+      timebase: string
+      aspect_ratio: string
       sar: string
       pix_fmt: string
       color_range: string
       color_space: string
       color_primaries: string
+      vlang: string
     of AudioKind:
+      acodec: string
       sampleRate: uint64
       channels: uint64
+      alang: string
+    of SubtitleKind:
+      scodec: string
+      slang: string
+    of ContainerKind:
+      idk: int64
 
   ReaderKind = enum
     unknownT,
-    unknownStreamT,
-    audioStreamT,
-    videoStreamT,
+    streamT,
     formatT,
 
 
+var
+  vtracks: uint64 = 0
+  atracks: uint64 = 0
+  stracks: uint64 = 0
+
 proc display_stream(stream: Stream) =
   if stream.kind == VideoKind:
-    echo &"""
- - codec: {stream.codec}
- - fps: {stream.fps}
- - resolution: {stream.width}x{stream.height}
- - pix_fmt: {stream.pix_fmt}
- - color range: {stream.color_range}
- - color space: {stream.color_space}
- - color primaries: {stream.color_primaries}
- - bitrate: {stream.bitrate}
- - lang: {stream.lang}
-"""
+    echo &"""   - track {vtracks - 1}:
+     - codec: {stream.vcodec}
+     - fps: {stream.fps}
+     - resolution: {stream.width}x{stream.height}
+     - aspect ratio: {stream.aspect_ratio}
+     - pixel aspect ratio: {stream.sar}
+     - duration: {stream.duration}
+     - pix_fmt: {stream.pix_fmt}
+     - color range: {stream.color_range}
+     - color space: {stream.color_space}
+     - color primaries: {stream.color_primaries}
+     - timebase: {stream.timebase}
+     - bitrate: {stream.bitrate}
+     - lang: {stream.vlang}"""
   elif stream.kind == AudioKind:
-    echo &"""
- - codec: {stream.codec}
- - samplerate: {stream.sampleRate}
- - channels: {stream.channels}
- - bitrate: {stream.bitrate}
- - lang: {stream.lang}
+    echo &"""   - track {atracks - 1}:
+     - codec: {stream.acodec}
+     - samplerate: {stream.sampleRate}
+     - channels: {stream.channels}
+     - duration: {stream.duration}
+     - bitrate: {stream.bitrate}
+     - lang: {stream.alang}"""
+  elif stream.kind == SubtitleKind:
+    echo &"""   - track {stracks - 1}:
+     - codec: {stream.scodec}
+     - lang: {stream.slang}"""
+  elif stream.kind == ContainerKind:
+    echo &""" - container:
+   - duration: {stream.duration}
+   - bitrate: {stream.bitrate}
 """
 
 proc info(args: seq[string]) =
@@ -65,7 +91,6 @@ proc info(args: seq[string]) =
     args=["-v", "-8", "-show_streams", "-show_format", args[1]],
     options={poUsePath}
   )
-  echo ffout
   var
     current = unknownT
     foo: seq[string]
@@ -76,15 +101,18 @@ proc info(args: seq[string]) =
     stream_defined: bool = false
 
   for line in splitLines(ffout):
-    if line.startswith("[/"):
-      current = unknownT
+    if line == "" or line.startswith("[/"):
       continue
-    if line == "[STREAM]":
-      current = unknownStreamT
-      continue
-    if line == "":
-      continue
+
     if line.startswith("["):
+      if line == "[STREAM]":
+        current = streamT
+      if line == "[FORMAT]":
+        if stream_defined:
+          display_stream(my_stream)
+        stream_defined = true
+        my_stream = Stream(kind: ContainerKind)
+        current = formatT
       continue
 
     if line.startswith("TAG:language="):
@@ -110,27 +138,44 @@ proc info(args: seq[string]) =
         display_stream(my_stream)
       stream_defined = true
       if val == "video":
-        my_stream = Stream(codec: codec_name, lang: "null", kind: VideoKind)
+        my_stream = Stream(kind: VideoKind, vcodec: codec_name, vlang: "null")
+        if vtracks == 0:
+          echo " - video:"
+        vtracks += 1
       elif val == "audio":
-        my_stream = Stream(codec: codec_name, lang: "null", kind: AudioKind)
+        my_stream = Stream(kind: AudioKind, acodec: codec_name, alang: "null")
+        if atracks == 0:
+          echo " - audio:"
+        atracks += 1
+      elif val == "subtitle":
+        my_stream = Stream(kind: SubtitleKind, scodec: codec_name, slang: "null")
+        if stracks == 0:
+          echo " - subtitle:"
+        stracks += 1
       else:
         raise newException(IOError, "Unknown codec type")
 
     if stream_defined:
-      if key == "lang":
-        my_stream.lang = val
       if key == "bit_rate":
         my_stream.bitrate = parseUInt(val)
       if key == "duration":
         my_stream.duration = val
 
       if my_stream.kind == VideoKind:
+        if key == "lang":
+          my_stream.vlang = val
         if key == "width":
           my_stream.width = parseUInt(val)
         if key == "height":
           my_stream.height = parseUInt(val)
         if key == "avg_frame_rate":
           my_stream.fps = val
+        if key == "sample_aspect_ratio":
+          my_stream.sar = val
+        if key == "display_aspect_ratio":
+          my_stream.aspect_ratio = val
+        if key == "time_base":
+          my_stream.timebase = val
 
         if key == "pix_fmt":
           my_stream.pix_fmt = val
@@ -140,11 +185,18 @@ proc info(args: seq[string]) =
           my_stream.color_space = val
         if key == "color_primaries":
           my_stream.color_primaries = val
+
       if my_stream.kind == AudioKind:
+        if key == "lang":
+          my_stream.alang = val
         if key == "sample_rate":
           my_stream.sampleRate = parseUInt(val)
         if key == "channels":
           my_stream.channels = parseUInt(val)
+
+      if my_stream.kind == SubtitleKind:
+        if key == "lang":
+          my_stream.slang = val
 
   if stream_defined:
     display_stream(my_stream)
