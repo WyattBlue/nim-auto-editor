695bf48d481459125e23593180078e1447a5b263
WyattBlue <wyattblue@auto-editor.com>
2023-07-04 14:16:25 -0400

    Allow fractional timeBase

--- src/sublevels.nim
+++ src/sublevels.nim
@@ -5,12 +5,13 @@ import std/memfiles
 import std/tempfiles
 import std/streams
 import std/strformat
+import std/rationals
 
 type
   Args = object
-    my_input: string
-    ff_loc: string
-    time_base: int
+    myInput: string
+    ffLoc: string
+    timeBase: Rational[int]
     stream: int
 
   WavContainer = object
@@ -29,9 +30,26 @@ proc error(msg: string) =
   stderr.writeLine(&"Error! {msg}")
   system.quit(1)
 
+func parseRational(val: string): Rational[int] =
+  let hmm = val.split("/", 1)
+
+  if len(hmm) == 1:
+    try:
+      return parseInt(hmm[0]) // 1
+    except CatchableError:
+      return 0//1
+
+  try:
+    let
+      num = parseInt(hmm[0])
+      den = parseInt(hmm[1])
+    return num // den
+  except CatchableError:
+    return 0//1
+
 proc vanparse(args: seq[string]): Args =
   var
-    my_args = Args(my_input:"", ff_loc:"ffmpeg", time_base:30, stream:0)
+    myArgs = Args(myInput:"", ffLoc:"ffmpeg", timeBase:0//1, stream:0)
     arg: string
     i = 1
 
@@ -55,29 +73,31 @@ Options:
       system.quit(1)
 
     elif arg == "--ffmpeg-location":
-      my_args.ff_loc = args[i+1]
+      myArgs.ffLoc = args[i+1]
       i += 1
     elif arg == "--edit":
       if not args[i+1].startswith("audio:"):
         error("`--edit` only supports audio method")
 
-      my_args.stream = parseInt(args[i+1][6 .. ^1])
+      myArgs.stream = parseInt(args[i+1][6 .. ^1])
       i += 1
     elif arg == "--timebase" or arg == "-tb":
-      my_args.time_base = parseInt(args[i+1])
-      if my_args.time_base < 1:
-        error("timebase must be greater than 0")
+      myArgs.timeBase = parseRational(args[i+1])
+      if myArgs.timeBase == 0//1:
+        error("Invalid timebase")
       i += 1
-    elif my_args.my_input != "":
+    elif myArgs.myInput != "":
       error("Only one file allowed")
     else:
-      my_args.my_input = arg
+      myArgs.myInput = arg
 
     i += 1
 
-  if my_args.my_input == "":
+  if myArgs.myInput == "":
     error("Input file required")
-  return my_args
+  if myArgs.timeBase == 0//1:
+    error("timebase must be set!")
+  return myArgs
 
 
 proc read(filename: string): WavContainer =
@@ -200,14 +220,14 @@ proc read(filename: string): WavContainer =
 proc levels(osargs: seq[string]) =
   let
     args = vanparse(osargs)
-    my_input = args.my_input
-    time_base = args.time_base
+    myInput = args.myInput
+    timeBase = args.timeBase
     dir = createTempDir("tmp", "")
     temp_file = joinPath(dir, "out.wav")
 
 
-  discard execProcess(args.ff_loc,
-    args=["-hide_banner", "-y", "-i", my_input, "-map", &"0:a:{args.stream}", "-rf64", "always", temp_file],
+  discard execProcess(args.ffLoc,
+    args=["-hide_banner", "-y", "-i", myInput, "-map", &"0:a:{args.stream}", "-rf64", "always", temp_file],
     options={poUsePath}
   )
 
@@ -216,7 +236,7 @@ proc levels(osargs: seq[string]) =
     mm = memfiles.open(temp_file, mode=fmRead)
     thres: seq[float64] = @[]
 
-  let samp_per_ticks = wav.sr div uint64(time_base) * wav.channels
+  let samp_per_ticks = uint64((int(wav.sr) / timeBase * int(wav.channels)).toInt())
 
   if wav.bytes_per_sample != 2:
     raise newException(IOError, "Expects int16 only")
