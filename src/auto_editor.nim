01fbb4b7f42650cdf532bc6f3a7b05c7a94b6479
WyattBlue <wyattblue@auto-editor.com>
2023-08-11 00:37:27 -0400

    Add minclip, mincut, and margin

--- src/auto_editor.nim
+++ src/auto_editor.nim
@@ -4,6 +4,8 @@ import std/osproc
 import std/rationals
 import std/strformat
 import std/tempfiles
+import std/enumerate
+import std/sequtils
 
 import subinfo
 import sublevels
@@ -43,16 +45,79 @@ discard execProcess("ffmpeg",
 
 let levels = getAudioThreshold(tempFile, 30//1, log)
 
+var hasLoud: seq[bool] = @[]
+for j in levels:
+  hasLoud.add(j >= 0.04)
+
+proc removeSmall(arr: var seq[bool], lim: int, replace, `with`: bool) =
+  var startP = 0
+  var active = false
+  for j, item in enumerate(arr):
+    if item == replace:
+      if not active:
+        startP = j
+        active = true
+
+      if j == len(arr) - 1 and j - startP < lim:
+        for i in startP ..< len(arr):
+          arr[i] = `with`
+    elif active:
+      if j - startP < lim:
+        for i in startP ..< j:
+          arr[i] = `with`
+      active = false
+
+# Apply minclip of 3
+removeSmall(hasLoud, 3, true, false)
+
+# Apply mincut of 6
+removeSmall(hasLoud, 6, false, true)
+
+# Apply margin of 0.2s 0.2s
+proc mutMargin(arr: var seq[bool], startM, endM: int) =
+  var startIndex, endIndex: seq[int]
+  let arrLen = len(arr)
+
+  for j in 1 ..< arrLen:
+    if arr[j] != arr[j - 1]:
+      if arr[j]:
+        startIndex.add(j)
+      else:
+        endIndex.add(j)
+
+  if startM > 0:
+    for i in startIndex:
+      let index = max(i - startM, 0) ..< i
+      arr[index] = repeat(true, index.len)
+  elif startM < 0:
+    for i in startIndex:
+      let index = i ..< min(i - startM, arrLen)
+      arr[index] = repeat(false, index.len)
+
+  if endM > 0:
+    for i in endIndex:
+      let index = i ..< min(i + endM, arrLen)
+      arr[index] = repeat(true, index.len)
+  elif endM < 0:
+    for i in endIndex:
+      let index = max(i + endM, 0) ..< i
+      arr[index] = repeat(false, index.len)
+
+mutMargin(hasLoud, 6, 6)
+
+
 var chunks: seq[(int, int, float)]
 var start = 0
-for j in 1 .. len(levels) - 1:
-  if (levels[j] > 0.04) != (levels[j - 1] > 0.04):
+for j in 1 ..< len(hasLoud):
+  if hasLoud[j] != hasLoud[j - 1]:
     chunks.add(
-      (start, j, (if levels[j - 1] > 0.04: 1.0 else: 0.0))
+      (start, j, (if hasLoud[j - 1]: 1.0 else: 0.0))
     )
     start = j
-chunks.add((start, len(levels), (if levels[len(levels) - 1] >
-    0.04: 1.0 else: 0.0)))
+
+chunks.add(
+  (start, len(hasLoud), (if hasLoud[^1]: 1.0 else: 0.0))
+)
 
 echo &"chunks: {chunks}"
 
